901ce76ee48772171beb2a1dde07391373af44fb
YoannBoisson <yoann.boisson@live.fr>
2022-01-12 11:52:43 +0100

    Finalisation des routes

--- level 2/back/server.js
+++ level 2/back/server.js
@@ -42,6 +42,7 @@ app.post("/report", (req, res) => {
                   console.log(idOwner);
                   res.status(500).send("Erreur dans la demande de signalement");
                 } else {
+                  const idBike = result.insertId;
                   db.query(
                     "SELECT id FROM officer WHERE disponibility=1 LIMIT 1",
                     (err, resultat) => {
@@ -52,7 +53,7 @@ app.post("/report", (req, res) => {
                         if (resultat.length == 0) {
                           //INSERER REPORT quand même
                           db.query(
-                            `INSERT INTO report (status) VALUES (1)`,
+                            `INSERT INTO report (status, id_bike) VALUES (1, ${idBike})`,
                             (err, result) => {
                               err ? console.log(err) : res.status(201);
                             }
@@ -65,7 +66,7 @@ app.post("/report", (req, res) => {
                         } else {
                           const idOfficer = resultat[0].id;
                           db.query(
-                            `INSERT INTO report (status, id_officer) VALUES (1, ${idOfficer})`,
+                            `INSERT INTO report (status, id_officer, id_bike) VALUES (1, ${idOfficer}, ${idBike})`,
                             (err, result) => {
                               if (err) {
                                 console.log(err);
@@ -113,16 +114,47 @@ app.post("/report", (req, res) => {
   );
 });
 
-//=== Voir les enquêtes en cours
-app.get("/report", (req, res) => {
-  db.query("SELECT * FROM report", (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.status(200).send(result);
+//=== Voir les enquêtes résolues
+app.get("/report/resolved", (req, res) => {
+  db.query(
+    "SELECT B.serial_number, R.status, O.id AS officerId FROM bike B INNER JOIN report R ON B.id = R.id_bike INNER JOIN officer O ON O.id = R.id_officer WHERE R.status=0",
+    (err, result) => {
+      if (err) {
+        res.status(500).send("Erreur dans la demande");
+      } else {
+        res.status(200).send(result);
+      }
     }
-  });
+  );
 });
+//=== Voir les enquêtes en cours
+app.get("/report/inProgress", (req, res) => {
+  db.query(
+    "SELECT B.serial_number, R.status, O.id AS officerId FROM bike B INNER JOIN report R ON B.id = R.id_bike INNER JOIN officer O ON O.id = R.id_officer WHERE R.status=1",
+    (err, result) => {
+      if (err) {
+        res.status(500).send("Erreur dans la demande");
+      } else {
+        res.status(200).send(result);
+      }
+    }
+  );
+});
+//=== Voir les enquêtes en attente de prise en charge par un policier
+app.get("/report/waiting", (req, res) => {
+  db.query(
+    "SELECT B.serial_number, R.status, R.id AS reportId FROM bike B INNER JOIN report R ON B.id = R.id_bike WHERE R.status=1 AND R.id_officer IS NULL",
+    (err, result) => {
+      if (err) {
+        res.status(500).send("Erreur dans la demande");
+      } else {
+        res.status(200).send(result);
+      }
+    }
+  );
+});
+
+// AJOUTER VELO associé
 
 //=== Report : mise à jour du statut d'enquête
 app.put("/report/:id", (req, res) => {
@@ -132,7 +164,7 @@ app.put("/report/:id", (req, res) => {
       res.status(500).send("Erreur dans la mise à jour des données");
     } else {
       db.query(
-        `SELECT id_officer FROM report R INNER JOIN officer O ON O.id=id_officer WHERE R.id=${reportId}`,
+        `SELECT id_officer FROM report R INNER JOIN officer O ON O.id=R.id_officer WHERE R.id=${reportId}`,
         (err, result) => {
           if (err) {
             console.log(err);
@@ -152,7 +184,11 @@ app.put("/report/:id", (req, res) => {
                     `UPDATE report SET id_officer=${officerId} WHERE status=1 AND id_officer IS NULL`,
                     (err, result) => {
                       if (err) {
-                        console.log(err);
+                        res
+                          .status(500)
+                          .send(
+                            "Erreur dans l'affectation du policier sur le dossier"
+                          );
                       } else {
                         res.status(200).send(result);
                       }
