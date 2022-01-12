73ec9c11771137388c5ef8ffb5abd2cb547adbf8
YoannBoisson <yoann.boisson@live.fr>
2022-01-12 16:19:01 +0100

    fetching data from back to front

--- level 2/front/src/pages/OfficerPage.jsx
+++ level 2/front/src/pages/OfficerPage.jsx
@@ -1,19 +1,84 @@
+import React, { useEffect, useState } from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemText from "@mui/material/ListItemText";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import FolderIcon from "@mui/icons-material/Folder";
+import DeleteIcon from "@mui/icons-material/Delete";
 import "./officer-page.css";
 
 export default function OfficerPage() {
+  const [dense, setDense] = React.useState(false);
+  const [secondary, setSecondary] = React.useState(false);
+
+  const [waiting, setWaiting] = useState([]);
+  const [inProgress, setInProgress] = useState([]);
+  const [resolved, setResolved] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/report/waiting")
+      .then((reponse) => reponse.json())
+      .then((result) => {
+        setWaiting(result);
+      });
+    fetch("http://localhost:3001/report/inProgress")
+      .then((reponse) => reponse.json())
+      .then((result) => {
+        setInProgress(result);
+      });
+    fetch("http://localhost:3001/report/resolved")
+      .then((reponse) => reponse.json())
+      .then((result) => {
+        setResolved(result);
+      });
+  }, []);
+
+  console.log(waiting, inProgress, resolved);
+
+  function generate(element) {
+    return [0, 1, 2].map((value) =>
+      React.cloneElement(element, {
+        key: value,
+      })
+    );
+  }
+
   return (
     <div className="page">
       <h1 className="title">Strasbourg Police Bike Dept.</h1>
       <div className="container">
-      <div className="sub-container">
-        <h2 className="subtitle">Menu</h2>
-        <p>Sélectionner une option</p>
-        <ul>
-          <li>Vélos volés</li>
-          <li>Affaires en cours</li>
-        </ul>
-      </div>
-      <div className="sub-container"></div>
+        <div className="sub-container">
+          <h2 className="subtitle">Menu</h2>
+          <p>Sélectionner une option</p>
+          <ul>
+            <li>Vélos volés</li>
+            <li>Affaires en cours</li>
+          </ul>
+        </div>
+        <div className="sub-container"></div>
+        <List dense={dense}>
+          {generate(
+            <ListItem
+              secondaryAction={
+                <IconButton edge="end" aria-label="delete">
+                  <DeleteIcon />
+                </IconButton>
+              }
+            >
+              <ListItemAvatar>
+                <Avatar>
+                  <FolderIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary="Single-line item"
+                secondary={secondary ? "Secondary text" : null}
+              />
+            </ListItem>
+          )}
+        </List>
       </div>
     </div>
   );
--- level 2/front/src/pages/ReportPage.jsx
+++ level 2/front/src/pages/ReportPage.jsx
@@ -1,114 +1,169 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import DirectionsBikeIcon from '@mui/icons-material/DirectionsBike';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
+import React, { useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import DirectionsBikeIcon from "@mui/icons-material/DirectionsBike";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
         Find your bike
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
 export default function ReportPage() {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [adress, setAdress] = useState("");
+  const [serialNumber, setSerialNumber] = useState("");
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    // eslint-disable-next-line no-console
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+    fetch("http://localhost:3001/report", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        firstname,
+        lastname,
+        email,
+        adress,
+        serialNumber,
+      }),
+    }).then((result) => {
+      console.log(result);
     });
   };
 
+  const handleChangeFirstname = (event) => {
+    event.preventDefault();
+    setFirstname(event.target.value);
+  };
+  const handleChangeLastname = (event) => {
+    event.preventDefault();
+    setLastname(event.target.value);
+  };
+  const handleChangeEmail = (event) => {
+    event.preventDefault();
+    setEmail(event.target.value);
+  };
+  const handleChangeAdress = (event) => {
+    event.preventDefault();
+    setAdress(event.target.value);
+  };
+  const handleChangeSerialNumber = (event) => {
+    event.preventDefault();
+    setSerialNumber(event.target.value);
+  };
+
+  console.log(firstname, lastname, email, adress, serialNumber);
+
   return (
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
-            <DirectionsBikeIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Signaler un vol
-          </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="Prénom"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  name="lastName"
-                  fullWidth
-                  id="lastName"
-                  label="Nom"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="bike"
-                  label="Numéro de série du vélo"
-                  id="serial"
-                  autoComplete="000000"
-                />
-              </Grid>
-
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
+          <DirectionsBikeIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Signaler un vol
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="firstname"
+                required
+                fullWidth
+                id="firstname"
+                label="Prénom"
+                onChange={handleChangeFirstname}
+                autoFocus
+                autoComplete="given-name"
+              />
             </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Signaler mon vélo
-            </Button>
-          </Box>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                name="lastname"
+                fullWidth
+                id="lastname"
+                label="Nom"
+                onChange={handleChangeLastname}
+                autoComplete="family-name"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label="Email"
+                name="email"
+                onChange={handleChangeEmail}
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="adress"
+                label="Adresse"
+                name="adress"
+                onChange={handleChangeAdress}
+                autoComplete="adresse"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="serialNumber"
+                label="Numéro de série du vélo"
+                id="serialNumber"
+                onChange={handleChangeSerialNumber}
+                autoComplete="000000"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Signaler mon vélo
+          </Button>
         </Box>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
-
+      </Box>
+      <Copyright sx={{ mt: 5 }} />
+    </Container>
   );
 }
-
